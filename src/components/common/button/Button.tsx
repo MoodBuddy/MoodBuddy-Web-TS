beb39b7ef200005689a3e00b3b9e7c26dba08185
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-09-07 21:56:54 +0900

    refactor: Button 컴포넌트
    - ComponentPropsWithRef 사용하여 타입지정 (forwardRef 대체)

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,30 +1,35 @@
-import { forwardRef } from 'react';
-import classNames from 'classnames';
+import { ComponentPropsWithRef } from "react";
+import classNames from "classnames";
 
-const Button = forwardRef(function Button(
-  { children, className, color = 'beige', ...props },
-  ref,
-) {
-  const buttonColors = {
-    brown: 'bg-[#AC7544] text-white hover:bg-[#8E5B33]',
-    beige: 'bg-[#D8B18E] text-black hover:bg-[#B08A5E]',
-    lightBeige: 'bg-[#F4EEE9] text-neutral-500 hover:bg-[#ECE6E0]',
-    white: 'bg-[#FFFEFD] text-neutral-500 hover:bg-[#DCC6B1]',
-  };
+interface ButtonProps extends ComponentPropsWithRef<"button"> {
+  color?: "brown" | "beige" | "lightBeige" | "white";
+}
 
+const buttonColors = {
+  brown: "bg-[#AC7544] text-white hover:bg-[#8E5B33]",
+  beige: "bg-[#D8B18E] text-black hover:bg-[#B08A5E]",
+  lightBeige: "bg-[#F4EEE9] text-neutral-500 hover:bg-[#ECE6E0]",
+  white: "bg-[#FFFEFD] text-neutral-500 hover:bg-[#DCC6B1]",
+} as const;
+
+const Button = ({
+  children,
+  className,
+  color = "beige",
+  ...props
+}: ButtonProps) => {
   return (
     <button
-      ref={ref}
       className={classNames(
-        'py-2 px-4 rounded-2xl font-semibold border',
+        "py-2 px-4 rounded-2xl font-semibold border",
         buttonColors[color],
-        className,
+        className
       )}
       {...props}
     >
       {children}
     </button>
   );
-});
+};
 
 export default Button;

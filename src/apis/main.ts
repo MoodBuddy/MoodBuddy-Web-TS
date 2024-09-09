ef6f06c759540a473dc8c3926f374adbe663b206
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-09-09 22:22:47 +0900

    refactor: main.ts

--- src/apis/main.ts
+++ src/apis/main.ts
@@ -1,24 +1,29 @@
-import client from './client';
+import { post } from "./apiUtils";
+import { BasicDiary, DiarySummaryData } from "@type/Diary";
+import { BaseResponse } from "@type/Api";
+import { END_POINT } from "@constants/api";
 
-const post = async (url, data) => {
-  const res = await client.post(url, data);
-  return res?.data;
-};
+type CalendarResponse = BaseResponse<BasicDiary>;
+type SummaryResponse = BaseResponse<DiarySummaryData>;
 
-export const postCalendar = async (month) => {
+export const postCalendar = async (month: {
+  calendarMonth: string;
+}): Promise<CalendarResponse> => {
   try {
-    const data = await post('/api/v1/member/main/month', month);
-    return data;
+    const res = await post<CalendarResponse>(END_POINT.USER.POST_MONTH, month);
+    return res.data;
   } catch (error) {
-    throw new Error('데이터 불러오기에 실패하였습니다.');
+    throw new Error("데이터 불러오기에 실패하였습니다.");
   }
 };
 
-export const postSummary = async (date) => {
+export const postSummary = async (date: {
+  calendarDay: string;
+}): Promise<SummaryResponse> => {
   try {
-    const data = await post('/api/v1/member/main/summary', date);
-    return data;
+    const res = await post<SummaryResponse>(END_POINT.USER.POST_SUMMARY, date);
+    return res.data;
   } catch (error) {
-    throw new Error('데이터 불러오기에 실패하였습니다.');
+    throw new Error("데이터 불러오기에 실패하였습니다.");
   }
 };

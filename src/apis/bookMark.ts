059d22ae1c5f50dedd0c56c7d97f6b7cc4f9c5e0
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-09-11 21:24:09 +0900

    refactor: bookMark.ts

--- src/apis/bookMark.ts
+++ src/apis/bookMark.ts
@@ -1,31 +1,30 @@
-import client from './client';
+import { END_POINT } from "@constants/api";
+import { get, post } from "./apiUtils";
+import { BaseResponse } from "@type/Api";
+import { BookMark, DetailedDiary } from "@type/Diary";
 
-const get = async (url) => {
-  const res = await client.get(url);
-  return res.data.data;
-};
-const post = async (url, data) => {
-  const res = await client.post(url, data);
-  return res?.data;
-};
+type BookMarkResponse = BaseResponse<DetailedDiary>;
+type BookMarkToggleResponse = BaseResponse<BookMark>;
 
-export const getBookMarkFindAll = async () => {
+export const getBookMarkFindAll = async (): Promise<BookMarkResponse> => {
   try {
-    const data = await get('/api/v1/member/bookMark/findAll?page=0&size=20');
-    return data;
+    const res = await get<DetailedDiary>(END_POINT.BOOKMARK.GET_ALL);
+    return res.data.data;
   } catch (error) {
-    throw new Error('데이터 불러오기에 실패하였습니다.');
+    throw new Error("데이터 불러오기에 실패하였습니다.");
   }
 };
 
-export const bookMarkToggle = async (diaryId) => {
+export const bookMarkToggle = async (
+  diaryId: number
+): Promise<BookMarkToggleResponse> => {
   try {
-    const data = await post(
-      `/api/v1/member/bookMark/toggle/${diaryId}`,
-      diaryId,
+    const res = await post<BookMark>(
+      END_POINT.BOOKMARK.POST_TOGGLE(diaryId),
+      diaryId
     );
-    return data;
+    return res.data;
   } catch (error) {
-    throw new Error('데이터 불러오기에 실패하였습니다.');
+    throw new Error("데이터 불러오기에 실패하였습니다.");
   }
 };

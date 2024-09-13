ae3caeb61ade1c2c3ee3f87f24650b5433de67eb
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-09-13 23:25:52 +0900

    refactor: 로그인 api 분리

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,16 +1,16 @@
 import { post } from "./apiUtils";
 import { END_POINT } from "@constants/api";
 import { BaseResponse } from "@type/Api";
-import { UserAuth } from "@type/Auth";
+import { AuthResponse, UserAuth } from "@type/Auth";
 
-type AuthResponse = BaseResponse<UserAuth>;
+type PostAuthResponse = BaseResponse<AuthResponse>;
 
 // 자체 로그인
 export const postTestLogin = async (
   userAuth: UserAuth
-): Promise<AuthResponse> => {
+): Promise<PostAuthResponse> => {
   try {
-    const res = await post<UserAuth>(END_POINT.USER.POST_LOGIN, userAuth);
+    const res = await post<AuthResponse>(END_POINT.USER.POST_LOGIN, userAuth);
     return res.data;
   } catch (error) {
     throw new Error("데이터 불러오기에 실패하였습니다.");
--- src/types/Auth.ts
+++ src/types/Auth.ts
@@ -1,3 +1,8 @@
 export interface UserAuth {
-  kakaoId: number;
+  kakaoId: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  refreshToken: string;
 }

8dd80afbf032ad8e2d0033019628f7664306a712
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-09-13 23:25:34 +0900

    feat: token 관련 util함수 정의

--- src/apis/utils.ts
+++ src/apis/utils.ts
@@ -1,13 +1,13 @@
 // Jwt 토큰 디코딩
 export const parseJwtPayload = (token: string) => {
   try {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/-/g, '/');
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
     const decodedPayload = decodeURIComponent(
       atob(base64)
-        .split('')
-        .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-        .join(''),
+        .split("")
+        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+        .join("")
     );
 
     return JSON.parse(decodedPayload);
@@ -15,3 +15,22 @@ export const parseJwtPayload = (token: string) => {
     return null;
   }
 };
+
+export function getAccessToken(): string | null {
+  const sessionData = sessionStorage.getItem("ACCESS_TOKEN");
+  return sessionData ? JSON.parse(sessionData).token : null;
+}
+
+export function getRefreshToken(): string | null {
+  return sessionStorage.getItem("REFRESH_TOKEN");
+}
+
+export function setTokens(accessToken: string, refreshToken: string) {
+  sessionStorage.setItem("ACCESS_TOKEN", JSON.stringify({ token: accessToken }));
+  sessionStorage.setItem("REFRESH_TOKEN", refreshToken);
+}
+
+export function removeTokens() {
+  sessionStorage.removeItem("ACCESS_TOKEN");
+  sessionStorage.removeItem("REFRESH_TOKEN");
+}

5f580bd56906c1481955ca163ccdc26eefee32b7
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-09-16 01:05:16 +0900

    refactor: 일기 검색 관련 api 분리 및 타입 지정
    - 추가로 불필요한 console.log() 제거

---
+++ src/apis/searchDiary.ts
@@ -0,0 +1,56 @@
+import { get } from "./apiUtils";
+import qs from "qs";
+import { DetailedDiary, DiaryEmotion } from "@type/Diary";
+import { PaginationResponse } from "@type/Api";
+import { END_POINT } from "@constants/api";
+import { SearchParams } from "@type/Search";
+
+type FindAllResponse = PaginationResponse<DetailedDiary>;
+
+export const getFindAll = async (): Promise<FindAllResponse> => {
+  try {
+    const res = await get<FindAllResponse>(END_POINT.DIARY.GET_FIND_ALL);
+    return res.data.data;
+  } catch (error) {
+    throw new Error("데이터 불러오기에 실패하였습니다.");
+  }
+};
+
+export const getFindAllByEmotion = async ({
+  emotion,
+}: DiaryEmotion): Promise<FindAllResponse> => {
+  try {
+    const res = await get<FindAllResponse>(
+      END_POINT.DIARY.GET_FIND_ALL_BY_EMOTION(emotion)
+    );
+    return res.data.data;
+  } catch (error) {
+    throw new Error("데이터 불러오기에 실패하였습니다.");
+  }
+};
+
+export const getFindAllByFilter = async (
+  searchParams: SearchParams
+): Promise<FindAllResponse> => {
+  try {
+    const url = END_POINT.DIARY.GET_FIND_ALL_BY_FILTER;
+    const params = {
+      keyWord: encodeURIComponent(searchParams.keyword),
+      page: 0,
+      size: 100,
+      sort: "string",
+      year: searchParams.year,
+      month: searchParams.month,
+      diarySubject: searchParams.topic,
+      diaryEmotion: searchParams.diaryEmotion,
+    };
+
+    // qs 사용, params 객체를 쿼리 문자열로 변환
+    const queryString = qs.stringify(params, { encode: false });
+
+    const res = await get<FindAllResponse>(`${url}?${queryString}`);
+    return res.data.data;
+  } catch (error) {
+    throw new Error("데이터 불러오기에 실패하였습니다.");
+  }
+};
